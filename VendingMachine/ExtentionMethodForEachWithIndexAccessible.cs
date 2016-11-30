be5dcd83ed1c6fa19003e79f06a633dc13e3d2a0
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-30 10:00:43 -0500

    Extension Method forEach style loop with access to index Extends interface IEnumerable

---
+++ VendingMachine/ExtentionMethodForEachWithIndexAccessible.cs
@@ -0,0 +1,43 @@
+﻿// using ExtensionMethods;
+
+
+
+
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+
+//             string[] products = {"Snickers Bar", "Puppy", "Cat", "Vegan Pizza", "Kevin Mack Sticker", "Vince Fabro Bobblehead", "SharePoint Rules Decal", "C# Coffee Mug", "Xbox One", "PS4"};
+//             products.Each((str, n) =>
+//             {
+//                 System.Console.Write("{0} at index {1}\n", str, n);
+
+//             });
+
+//         }
+//     }
+// }
+
+// namespace ExtensionMethods
+// {
+//     using System;
+//     using System.Collections.Generic;
+
+//     public static class MyExtensions
+//     {
+//         public static int WordCount(this String str)
+//         {
+//             return str.Split(new char[] { ' ', '.', '?' },
+//                              StringSplitOptions.RemoveEmptyEntries).Length;
+//         }
+
+//         public static void Each<T>(this IEnumerable<T> ie, Action<T, int> action)
+//         {
+//             var i = 0;
+//             foreach (var e in ie) action(e, i++);
+//         }
+//     }
+// }
--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,45 +1,25 @@
-﻿using System;
-using ExtensionMethods;  
+﻿// using System;
 
 
 
-namespace ConsoleApplication
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
 
-            string s = "Hello Extension Methods";
-            int i = s.WordCount();
-
-
-            System.Console.Write(i);
+  
 
 
+//             // string[] products = {"Snickers Bar", "Puppy", "Cat", "Vegan Pizza", "Kevin Mack Sticker", "Vince Fabro Bobblehead", "SharePoint Rules Decal", "C# Coffee Mug", "Xbox One", "PS4"};
 
+//             // foreach (string product in products)
+//             // {
+//             //     System.Console.Write("");
+//             // }
 
-
-            // string[] products = {"Snickers Bar", "Puppy", "Cat", "Vegan Pizza", "Kevin Mack Sticker", "Vince Fabro Bobblehead", "SharePoint Rules Decal", "C# Coffee Mug", "Xbox One", "PS4"};
-
-            // foreach (string product in products)
-            // {
-            //     System.Console.Write("");
-            // }
-
-            // Console.WriteLine(products[10]);
-        }
-    }
-}
-
-namespace ExtensionMethods
-{
-    public static class MyExtensions
-    {
-        public static int WordCount(this String str)
-        {
-            return str.Split(new char[] { ' ', '.', '?' },
-                             StringSplitOptions.RemoveEmptyEntries).Length;
-        }
-    }
-}
+//             // Console.WriteLine(products[10]);
+//         }
+//     }
+// }
