044d364c56a208aafe78a969150460cd1d36c305
Adam Pasternack <adam.pasternack@gmail.com>
2016-11-30 09:17:44 -0500

    Implementing Extension Methods for a Class

--- VendingMachine/Program.cs
+++ VendingMachine/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ExtensionMethods;  
+
+
 
 namespace ConsoleApplication
 {
@@ -6,12 +9,37 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            
-            string[] products = {"Snickers Bar", "Puppy", "Cat", "Vegan Pizza", "Kevin Mack Sticker", "Vince Fabro Bobblehead", "SharePoint Rules Decal", "C# Coffee Mug", "Xbox One", "PS4"};
-            
-            
-            
-            Console.WriteLine(products[10]);
+
+            string s = "Hello Extension Methods";
+            int i = s.WordCount();
+
+
+            System.Console.Write(i);
+
+
+
+
+
+            // string[] products = {"Snickers Bar", "Puppy", "Cat", "Vegan Pizza", "Kevin Mack Sticker", "Vince Fabro Bobblehead", "SharePoint Rules Decal", "C# Coffee Mug", "Xbox One", "PS4"};
+
+            // foreach (string product in products)
+            // {
+            //     System.Console.Write("");
+            // }
+
+            // Console.WriteLine(products[10]);
+        }
+    }
+}
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                             StringSplitOptions.RemoveEmptyEntries).Length;
         }
     }
 }
---
+++ VendingMachine/ProgramExtentionMethodsExtendingAClass.cs
@@ -0,0 +1,33 @@
+﻿// using System;
+// using ExtensionMethods;  
+
+
+
+// namespace ConsoleApplication
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+
+//             string s = "Hello Extension Methods";
+//             int i = s.WordCount();
+
+
+//             System.Console.Write(i);
+
+//         }
+//     }
+// }
+
+// namespace ExtensionMethods
+// {
+//     public static class MyExtensions
+//     {
+//         public static int WordCount(this String str)
+//         {
+//             return str.Split(new char[] { ' ', '.', '?' },
+//                              StringSplitOptions.RemoveEmptyEntries).Length;
+//         }
+//     }
+// }
