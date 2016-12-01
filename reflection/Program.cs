bdae9fce1175a3a2cc389ca96aa5076dd8c4b22a
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-01 10:56:05 -0500

    using .GetType() and typeof() to get type of instantiated and static classes

--- reflection/Program.cs
+++ reflection/Program.cs
@@ -7,27 +7,12 @@ namespace ReflectionTest
 {
     class Program
     {
-        private static int a = 5, b = 10, c = 20;
-
         static void Main(string[] args)
         {
-            Console.WriteLine("a + b + c = " + (a + b + c));
-            Console.WriteLine("Please enter the name of the variable that you wish to change:");
-            string varName = Console.ReadLine();
-            Type t = typeof(Program);
-            FieldInfo fieldInfo = t.GetField(varName, BindingFlags.NonPublic | BindingFlags.Static);
-            if(fieldInfo != null)
-            {
-                Console.WriteLine("The current value of " + fieldInfo.Name + " is " + fieldInfo.GetValue(null) + ". You may enter a new value now:");
-                string newValue = Console.ReadLine();
-                int newInt;
-                if(int.TryParse(newValue, out newInt))
-                {
-                    fieldInfo.SetValue(null, newInt);
-                    Console.WriteLine("a + b + c = " + (a + b + c));
-                }
-                Console.ReadKey();
-            }
+            string test = "test";
+            Console.WriteLine(test.GetType().FullName);
+            Console.WriteLine(typeof(Int32).FullName);
+            Console.ReadKey();
         }
     }
 }
