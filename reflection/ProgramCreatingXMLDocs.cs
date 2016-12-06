f49271031d30957c757031f458c5c269c9c8cc91
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-05 21:05:37 -0500

    creating XML asp net

---
+++ reflection/ProgramCreatingXMLDocs.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+using System.Xml;
+
+namespace ParsingXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+            foreach(XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
+                Console.WriteLine(xmlNode.Attributes["currency"].Value + ": " + xmlNode.Attributes["rate"].Value);
+            Console.ReadKey();
+        }
+    }
+}
--- reflection/ProgramReadingXML.cs
+++ reflection/ProgramReadingXML.cs
@@ -1,23 +1,25 @@
-using System;
-using System.Text;
-using System.Xml;
+// NEEDS CONVERTED TO CORE 
 
-namespace ParsingXml
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {            
-            XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
-            while(xmlReader.Read())
-            {
-                if((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
-                {
-                    if(xmlReader.HasAttributes)
-                        Console.WriteLine(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));                    
-                }
-            }
-            Console.ReadKey();
-        }
-    }
-}
+//using System;
+// using System.Text;
+// using System.Xml;
+
+// namespace ParsingXml
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {            
+//             XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+//             while(xmlReader.Read())
+//             {
+//                 if((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+//                 {
+//                     if(xmlReader.HasAttributes)
+//                         Console.WriteLine(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));                    
+//                 }
+//             }
+//             Console.ReadKey();
+//         }
+//     }
+// }
