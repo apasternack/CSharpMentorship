c93c0eefe9d2af0e6a0006fc93ad3330bb9f221b
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-05 21:07:05 -0500

    euro xml doc

--- reflection/ProgramCreatingXMLDocs.cs
+++ reflection/ProgramCreatingXMLDocs.cs
@@ -1,18 +1,19 @@
-using System;
-using System.Text;
-using System.Xml;
+// NEEDS CONVERTED TO .NET CORE
+// using System;
+// using System.Text;
+// using System.Xml;
 
-namespace ParsingXml
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
-            foreach(XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
-                Console.WriteLine(xmlNode.Attributes["currency"].Value + ": " + xmlNode.Attributes["rate"].Value);
-            Console.ReadKey();
-        }
-    }
-}
+// namespace ParsingXml
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             XmlDocument xmlDoc = new XmlDocument();
+//             xmlDoc.Load("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+//             foreach(XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
+//                 Console.WriteLine(xmlNode.Attributes["currency"].Value + ": " + xmlNode.Attributes["rate"].Value);
+//             Console.ReadKey();
+//         }
+//     }
+// }
