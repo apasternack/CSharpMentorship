ee4dcd42a9aa5684149df070fecb58666036692c
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-05 21:03:36 -0500

    reading XML

---
+++ reflection/ProgramReadingXML.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text;
+using System.Xml;
+
+namespace ParsingXml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+            while(xmlReader.Read())
+            {
+                if((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+                {
+                    if(xmlReader.HasAttributes)
+                        Console.WriteLine(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));                    
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
