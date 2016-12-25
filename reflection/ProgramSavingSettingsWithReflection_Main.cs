2dc25dd7b66e628526efb69028d3032989ec1881
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-24 22:26:18 -0500

    delegate fun

--- reflection/ProgramSavingSettingsWithReflection_Main.cs
+++ reflection/ProgramSavingSettingsWithReflection_Main.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Reflection;
+﻿// using System;
+// using System.Reflection;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Person person = new Person();
-        person.Load();
-        if((person.Age > 0) && (person.Name != String.Empty))
-        {
-            Console.WriteLine("Hi " + person.Name + " - you are " + person.Age + " years old!");
-        }
-        else
-        {
-            Console.WriteLine("I don't seem to know much about you. Please enter the following information:");
-            Type type = typeof(Person);
-            PropertyInfo[] properties = type.GetProperties();
-            foreach(PropertyInfo propertyInfo in properties)
-            {
-                Console.WriteLine(propertyInfo.Name + ":");
-                person.SetProperty(propertyInfo, Console.ReadLine());
-            }
-            person.Save();
-            Console.WriteLine("Thank you! I have saved your information for next time.");
-        }
-        Console.ReadKey();
-    }
-}
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Person person = new Person();
+//         person.Load();
+//         if((person.Age > 0) && (person.Name != String.Empty))
+//         {
+//             Console.WriteLine("Hi " + person.Name + " - you are " + person.Age + " years old!");
+//         }
+//         else
+//         {
+//             Console.WriteLine("I don't seem to know much about you. Please enter the following information:");
+//             Type type = typeof(Person);
+//             PropertyInfo[] properties = type.GetProperties();
+//             foreach(PropertyInfo propertyInfo in properties)
+//             {
+//                 Console.WriteLine(propertyInfo.Name + ":");
+//                 person.SetProperty(propertyInfo, Console.ReadLine());
+//             }
+//             person.Save();
+//             Console.WriteLine("Thank you! I have saved your information for next time.");
+//         }
+//         Console.ReadKey();
+//     }
+// }
